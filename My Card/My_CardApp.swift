80f7732420a9563cd23987389f70e265185c4dea
tin <tin.pt@ntnc.vn>
2023-06-24 19:15:17 +0700

    Initial Commit

---
+++ My Card/My_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CardApp.swift
+//  My Card
+//
+//  Created by Administrator on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct My_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
